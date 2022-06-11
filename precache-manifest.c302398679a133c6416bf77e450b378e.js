ff9159b7001fc3e4d059c7e4b49a10d331e34d6d
flash221 <rahul.vada@gmail.com>
2022-06-12 01:17:58 +0530

    Updates

---
+++ precache-manifest.c302398679a133c6416bf77e450b378e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b8dba4ffa9958e6c11c",
+    "url": "/travel-journal/static/css/main.396d51e1.chunk.css"
+  },
+  {
+    "revision": "6b8dba4ffa9958e6c11c",
+    "url": "/travel-journal/static/js/main.6b8dba4f.chunk.js"
+  },
+  {
+    "revision": "24e3c9f1bab9ef95748a",
+    "url": "/travel-journal/static/js/1.24e3c9f1.chunk.js"
+  },
+  {
+    "revision": "6b67c87f7fe4c66fb3a7",
+    "url": "/travel-journal/static/js/runtime~main.6b67c87f.js"
+  },
+  {
+    "revision": "f3bc2d473ddecf1f4b144122e69a9ace",
+    "url": "/travel-journal/index.html"
+  }
+];
